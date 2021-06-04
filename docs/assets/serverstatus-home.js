98fb815d78a5f26169eab59c2255175e9989aaec
vertilo <vertilo.dev@gmail.com>
2021-06-05 00:53:53 +0300

    -

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -8,14 +8,14 @@
         // siteNav dependent
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Играй сейчас: ${data.players.online} Online</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Мы онлайн!&ensp;&middot;&ensp;${data.players.online} играет</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Онлайн, ${data.players.online} играет</p>`;
         {% endif %}
     }
     else {
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Currently Offline</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Сейчас оффлайн</span>`;
         document.getElementById("serverIP").classList.remove("bg-green-900");
         document.getElementById("serverIP").classList.add("bg-orange-800");
 
@@ -23,8 +23,8 @@
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Простите! Мы оффлайн...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Оффлайн</p>`;
         {% endif %}
     }
 });
--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -7,16 +7,16 @@
         document.getElementById("serverInd").classList.add("text-green-600");
         document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Мы онлайн!&ensp;&middot;&ensp;${data.players.online} играет</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Онлайн, ${data.players.online} играет</p>`;
         {% endif %}
     }
     else {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Простите! Мы оффлайн...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Оффлайн</p>`;
         {% endif %}
     }
 });
